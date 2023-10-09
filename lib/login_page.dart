105a3a3aeace577f588c0dc9ae437a46fcff009a
CRkalpande <kalpandechaitanya@gmail.com>
2023-10-09 08:27:17 +0530

    login

---
+++ lib/login_page.dart
@@ -0,0 +1,109 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool passwordVisible = false;
+
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: SafeArea(
+          child: Form(
+            key: _formkey,
+            child: Column(children: [
+              Image.asset("assets/images/login_image.png"),
+              Text(
+                "Welcome",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter UserName",
+                        labelText: "UserName",
+                      ),
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Username cannot be Empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: !passwordVisible,
+                      decoration: InputDecoration(
+                          hintText: " Enter Password",
+                          labelText: "Password",
+                          suffixIcon: IconButton(
+                              icon: Icon(passwordVisible
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                setState(() {
+                                  passwordVisible = !passwordVisible;
+                                });
+                              })),
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Password cannot be Empty";
+                        } else if (value != null && value.length < 6) {
+                          return "password length should be atleat 6";
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {}
+                    },
+                    style: ElevatedButton.styleFrom(fixedSize: Size(125, 50)),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+              ),
+              RichText(
+                  text: TextSpan(children: [
+                TextSpan(
+                    text: "Don't have an account ? ",
+                    style: TextStyle(color: Colors.black)),
+                TextSpan(
+                    text: "Sign up",
+                    style: TextStyle(color: Colors.blue),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        // print("sign up");
+                      })
+              ]))
+            ]),
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:petcare/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:petcare/utils/routes.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute:  MyRoutes.loginRoute,
+      routes: {
+        "/":(context) => LoginPage(),
+        MyRoutes.loginRoute:(context) => LoginPage(),
+      
+      },
+    );
+  }
+}
